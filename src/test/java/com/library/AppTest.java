e49683a31f34162d67bbc057d6bf18d168fab6e2
Alex Gabrichidze <gabrichteam@gmail.com>
2025-01-02 23:33:35 -0600

    Initial commit

---
+++ src/test/java/com/library/AppTest.java
@@ -0,0 +1,12 @@
+package com.library;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void testApp() {
+        System.out.println("FUCKING WORKING HELL YEAH");
+        assertTrue(true);
+    }
+}
