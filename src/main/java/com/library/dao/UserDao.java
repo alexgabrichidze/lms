24ce68af5c30fb05989bc0f735974154e763f9d9
Alex Gabrichidze <gabrichteam@gmail.com>
2025-01-06 18:46:18 -0600

    Created UserDao interface.

---
+++ src/main/java/com/library/dao/UserDao.java
@@ -0,0 +1,59 @@
+package com.library.dao;
+
+import com.library.model.User;
+
+import java.util.List;
+
+/**
+ * Interface for managing user-related database operations.
+ * Provides methods to perform CRUD operations and other queries on the users
+ * table.
+ */
+public interface UserDao {
+
+    /**
+     * Adds a new user to the database.
+     * 
+     * @param user the User object containing the details of the user to be added
+     */
+    void addUser(User user);
+
+    /**
+     * Retrieves a user from the database by their unique ID.
+     * 
+     * @param id the ID of the user to retrieve
+     * @return the User object if found, or null if no user with the given ID exists
+     */
+    User getUserById(int id);
+
+    /**
+     * Retrieves all users from the database.
+     * 
+     * @return a list of User objects, or an empty list if no users are found
+     */
+    List<User> getAllUsers();
+
+    /**
+     * Updates the details of an existing user in the database.
+     * 
+     * @param user the User object containing the updated details
+     */
+    void updateUser(User user);
+
+    /**
+     * Deletes a user from the database by their unique ID.
+     * 
+     * @param id the ID of the user to delete
+     */
+    void deleteUser(int id);
+
+    /**
+     * Retrieves a user from the database by their unique email address.
+     * This method can be useful for login or authentication purposes.
+     * 
+     * @param email the email address of the user to retrieve
+     * @return the User object if found, or null if no user with the given email
+     *         exists
+     */
+    User getUserByEmail(String email);
+}
