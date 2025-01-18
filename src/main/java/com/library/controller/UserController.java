1e9485194f033591ad94a53541755d37dac84a0e
Alex Gabrichidze <gabrichteam@gmail.com>
2025-01-17 21:17:15 -0600

    Initially set up the user controller class.

---
+++ src/main/java/com/library/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.library.controller;
+
+import com.library.service.UserService;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class UserController extends BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private static UserService userService;
+}
